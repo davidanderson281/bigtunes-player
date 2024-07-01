3fa1c050410fe2eaa1233b452710c55a3978ad8f
David Anderson <43776319+davidanderson281@users.noreply.github.com>
2024-07-01 13:04:46 +0100

    add storage backend

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import 'tailwindcss/tailwind.css';
 import './index.css';
 import App from './App';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
